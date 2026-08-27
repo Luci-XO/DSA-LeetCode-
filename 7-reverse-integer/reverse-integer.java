67a6381aa256d82bd3cf0f7e0da50751c7974ad4
Ayan Bhardwaj <114657691+Luci-XO@users.noreply.github.com>
2026-08-27 14:29:22 +0530

    Time: 1 ms (99.98%) | Memory: 42.5 MB (80.29%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        
+        long r=0;
+        while(x!=0){
+            r*=10;
+            r+=(x%10);
+            x/=10;
+        }if(r>Integer.MAX_VALUE || r<Integer.MIN_VALUE) return 0;
+        return (int)r;
+    }
+}
